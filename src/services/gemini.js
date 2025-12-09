b7c4444dee74491a13b04e1f708deda4c0d64a47
Saurabh Tiwari <st108113@gmail.com>
2025-12-09 22:08:45 +0530

    fixed gemini model

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -6,13 +6,13 @@ import {
 import { config } from '../config.js';
 
 
-const genAI = new GoogleGenAI({apiKey: config.geminiKey});
+const genAI = new GoogleGenAI({ apiKey: config.geminiKey });
 
 
 
 const Config = {
   systemInstruction: "You are a multimodel Ai bot that can answer user questions based on their text images documents or audios ",
-  tools: [{googleSearch: {}}]
+  tools: [{ googleSearch: {} }]
 };
 
 
@@ -30,16 +30,16 @@ export async function getGeminiResponse(messages) {
 
     // Send request with function declarations
     const finalResponse = await genAI.models.generateContent({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-2.5-flash',
       contents: contents,
       config: Config,
       tools: Config.tools
     });
-   
-     // Return the text from the final response
-      return finalResponse.candidates[0].content.parts[0].text;
-    } 
-   catch (error) {
+
+    // Return the text from the final response
+    return finalResponse.candidates[0].content.parts[0].text;
+  }
+  catch (error) {
     console.error('Gemini API Error:', error);
     throw error;
   }
@@ -48,7 +48,7 @@ export async function getGeminiResponse(messages) {
 export async function analyzeImageWithGemini(imageData, prompt) {
   try {
     const response = await genAI.models.generateContent({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-2.5-flash',
       contents: [
         { text: prompt || "What's in this image?" },
         { inlineData: { data: imageData, mimeType: "image/jpeg" } }
@@ -65,7 +65,7 @@ export async function analyzeImageWithGemini(imageData, prompt) {
 export async function analyzeFileWithGemini(fileBuffer, mimeType, prompt) {
   try {
     const response = await genAI.models.generateContent({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-2.5-flash',
       contents: [
         {
           inlineData: {
